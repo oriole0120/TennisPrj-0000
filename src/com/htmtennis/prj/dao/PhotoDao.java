5bc661621dbe5243b8c63e4b31b26cc29cf411ff
deian <oriole0120@naver.com>
2015-03-02 16:35:13 +0900

    photoDao 연결만 성공

--- src/com/htmtennis/prj/dao/PhotoDao.java
+++ src/com/htmtennis/prj/dao/PhotoDao.java
@@ -11,15 +11,15 @@ public interface PhotoDao {
 	public Photo nextPhoto(String curCode);
 	
 	public List<Photo> getPhotos(int page, String query, String field);
-	/*public List<Photo> getPhotos(int page, String query);
-	public List<Photo> getPhotos(int page);*/
+	public List<Photo> getPhotos(int page, String query);
+	public List<Photo> getPhotos(int page);
 
-//	public int insert(Photo photo);
+	public int insert(Photo photo);
 	public int update(Photo photo);
 	public int delete(String code);
 	
 	public int getSize(String query, String field);
-//	public int getSize(String query);
+	public int getSize(String query);
 	public String lastCode();
 
 }
