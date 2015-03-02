e89a548f71064c467b2357cbacf28357c96ecae4
faceshop <sinbi050@gmail.com>
2015-03-02 16:42:39 +0900

    Link commit

---
+++ src/com/htmtennis/prj/model/Court.java
@@ -0,0 +1,43 @@
+package com.htmtennis.prj.model;
+
+
+
+public class Court {
+	private int code;
+	private String name;
+	private String address;
+	private String phoNum;
+	private String site;
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoNum() {
+		return phoNum;
+	}
+	public void setPhoNum(String phoNum) {
+		this.phoNum = phoNum;
+	}
+	public String getSite() {
+		return site;
+	}
+	public void setSite(String site) {
+		this.site = site;
+	}
+	
+	
+}
